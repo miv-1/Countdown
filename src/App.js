b1f83de2341d76f8711b299186f5ddd8a26b420f
Václav Míka <vaclav.mika14@gmail.com>
2022-06-20 11:55:49 +0200

    react update

--- src/App.js
+++ src/App.js
@@ -3,83 +3,21 @@ import CountdownTimer from "./components/CountdownTimer/CountdownTimer";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import './bootstrap.min.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from "./Pages/Home";
+import HelloThere from "./Pages/HelloThere";
 
 function App() {
-    const myDate = "2022-07-01"
-    const timestamp = +new Date(myDate)
-
-    const retirement = "2047-09-01"
-    const timestamp2 = +new Date(retirement)
-
-    const zippy = "2040-09-01"
-    const timestamp3 = +new Date(zippy)
-
     return (
-  <div>
-      <Header/>
-       <div className="container-fluid">
-           <div className='row'>
-           <div className="col rounded bg-gradient-1 text-white shadow p-5 text-center mb-5">
-               <p className="mb-4 font-weight-bold text-uppercase">Remaining time on PS-CC/EAG</p>
-               <div id="clock-b" className="countdown-circles d-flex flex-wrap justify-content-center pt-4">
-                   <CountdownTimer
-                       countdownTimestampMs={timestamp}/>
-               </div>
-
-
-            </div>
-
-           <div className="col rounded bg-gradient-1 text-white shadow p-5 text-center mb-5">
-               <p className="mb-4 font-weight-bold text-uppercase">Remaining time in CZ</p>
-               <div id="clock-b" className="countdown-circles d-flex flex-wrap justify-content-center pt-4">
-                   <CountdownTimer
-                       countdownTimestampMs={timestamp}/>
-               </div>
-
-
-           </div>
-           <div className="col rounded bg-gradient-1 text-white shadow p-5 text-center mb-5">
-               <p className="mb-4 font-weight-bold text-uppercase">Retirement</p>
-               <div id="clock-b" className="countdown-circles d-flex flex-wrap justify-content-center pt-4">
-                   <CountdownTimer
-                       countdownTimestampMs={timestamp2}/>
-               </div>
-
-
-           </div>
-           <div className="col rounded bg-gradient-1 text-white shadow p-5 text-center mb-5">
-               <p className="mb-4 font-weight-bold text-uppercase">Nourish </p>
-               <div id="clock-b" className="countdown-circles d-flex flex-wrap justify-content-center pt-4">
-                   <CountdownTimer
-                       countdownTimestampMs={timestamp3}/>
-               </div>
-
-
-           </div>
-           <div className="col rounded bg-gradient-1 text-white shadow p-5 text-center mb-5">
-               <p className="mb-4 font-weight-bold text-uppercase">another event</p>
-               <div id="clock-b" className="countdown-circles d-flex flex-wrap justify-content-center pt-4">
-                   <CountdownTimer
-                       countdownTimestampMs={timestamp2}/>
-               </div>
-
-
-           </div>
-           <div className="col rounded bg-gradient-1 text-white shadow p-5 text-center mb-5">
-               <p className="mb-4 font-weight-bold text-uppercase">another event</p>
-               <div id="clock-b" className="countdown-circles d-flex flex-wrap justify-content-center pt-4">
-                   <CountdownTimer
-                       countdownTimestampMs={timestamp2}/>
-               </div>
-
-
-           </div>
-
-       </div>
-       </div>
-      <Footer/>
-  </div>
-  );
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home/>}/>
+                <Route path='/home' element={<Home/>}/>
+                <Route path='/HelloThere' element={<HelloThere/>}/>
+                <Route path='*' element={<h1>This page does not exist.</h1>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,14 +9,14 @@ function Header() {
       <header>
         <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
           <Container>
-            <Navbar.Brand href="/">Countdown</Navbar.Brand>
+            <Navbar.Brand href="/">Ivan's countdown</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
                 <Nav.Link href="/">Home</Nav.Link>
               </Nav>
               <Nav className="mr-auto header;">
-                <Nav.Link href="/new">New Date</Nav.Link>
+                <Nav.Link href="/HelloThere">Hello there</Nav.Link>
               </Nav>
               <Nav className="justify-content-end" style={{ width: "100%" }}>
               </Nav>
