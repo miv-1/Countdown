494392bc6a2256a4eb9807b279af58ffef43d445
miv-1 <81264567+miv-1@users.noreply.github.com>
2022-06-23 10:21:15 +0200

    Update HelloThere.js

--- src/Pages/HelloThere.js
+++ src/Pages/HelloThere.js
@@ -19,7 +19,7 @@ function HelloThere() {
                   "I was studying frequency in my physics class. Now my brain Hertz."
               </p>
             <p>
-              Thank you boss for staying with us for 5 years, for your support. We have enjoyed working with you! Good luck with your new position. Perhaps our paths will cross again in the future.  <a href="/r" className="alert-link">Would like to see GPA going to moon again? Click on this link!
+              Thank you boss for staying with us for 5 years, for your support. We have enjoyed working with you! Good luck with your new position. Perhaps our paths will cross again in the future.  <a href="/GPA" className="alert-link">Would like to see GPA going to moon again? Click on this link!
               </a>.
             </p>
             </div>
